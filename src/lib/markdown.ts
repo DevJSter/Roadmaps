86453179895072439c59e1237c65c750a47b70bf
Shubham Tiwari <shubht3303@gmail.com>
2024-01-12 19:20:25 +0530

    fixes : structure

--- src/lib/markdown.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import MarkdownIt from 'markdown-it';
-
-export function markdownToHtml(markdown: string): string {
-  const md = new MarkdownIt();
-
-  return md.renderInline(markdown);
-}
